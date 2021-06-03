e89566c7f4651bae4dab12b71b5eafa0627a60fc
rindi41420110041 <85298572+rindi41420110041@users.noreply.github.com>
2021-06-03 21:35:46 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,27 +6,76 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1846802217:AAFjAnyjNw4WXxy2KcQco5YY4wRqin2GII4'
 const bot = new TelegramBot(token, {polling: true});
 
 
 // bots
+state = 0;
+// Main Menu Bot
 bot.onText(/\/start/, (msg) => { 
-    console.log(msg)
+    //console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
-    );   
+        click /predict`
+    );
+	state = 0;
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+bot.onText(/\/predict/, (msg) => { 
+    //console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `masukkan nilai i|v contoh 9|9`
     );   
+    state = 1
 });
+
+//bot.on('message', (msg) => {
+//	if(state == 1){
+//		console.log(msg.Text);		
+//  	}else {
+//        	state = 0 
+//        }
+//})
+
+bot.on('message', (msg) => {
+	if(state == 1){
+		//console.log(msg.Text);
+		s = msg.text.split("|");
+		//i = parseFloat(s[0])
+		//r = parseFloat(s[1])
+		model.predict(
+			[
+				parseFloat(s[0]),
+				parseFloat(s[1])
+			]
+		).then((jres1)=>{
+			//v = parseFloat(jres1[0])
+			//p = parseFloat(jres1[1])
+			
+			cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
+						
+			bot.sendMessage(
+				msg.chat.id,
+				`nilai v yang diprediksi adalah ${jres1[0]} volt`
+			);
+			bot.sendMessage(
+				msg.chat.id,
+				`nilai p yang diprediksi adalah ${jres1[1]} watt`
+			);
+			bot.sendMessage(
+				msg.chat.id,
+				`klasifikasi tegangan ${jres2}`
+			);
+			state = 0
+		})
+	})
+    }else {	   
+        state = 0 
+        }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
@@ -38,6 +87,26 @@ r.get('/prediction/:i/:r', function(req, res, next) {
     ).then((jres)=>{
         res.json(jres);
     })
+});
+
+r.get('/classify/:i/:r', function(req, res, next){
+	model.predict(
+		[
+			parseFLoat(req.params.i),
+			parseFloat(req.params.r)
+		]
+	).then((jres)=>{
+		cls_model.classify(
+			[
+				parseFloat(req.params.i),
+				parseFloat(req.params.r),
+				parseFloat(jres[0]),
+				parseFloat(jres[1])
+			]
+		).then((jres_)=>{
+			res.json({jres_})
+		})
+	})
 });
 
 module.exports = r;
